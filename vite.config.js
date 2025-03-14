a20c94624d1aa6c261e13113f77fc2608ba3d17d
artiom mezheynikov <artiom.mezheynikov.work@gmail.com>
2025-03-14 04:53:39 +0300

    test 3

--- vite.config.js
+++ vite.config.js
@@ -1,3 +1,16 @@
+import { defineConfig } from 'vite';
+
 export default {
-	base: '/vite-test-project'
+	base: '/vite-test-project',
+	css: {
+		devSourcemap: true, // Включить source maps для CSS/SCSS в режиме разработки
+		preprocessorOptions: {
+			scss: {
+				sourceMap: true, // Включить source maps для SCSS
+			},
+		},
+	},
+	build: {
+		sourcemap: true, // Включить source maps для продакшена
+	},
 }
